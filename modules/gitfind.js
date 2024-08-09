85d4110916d2d8ad3c9ea4d767ecd1ac6fe3f488
Risto Ikonen <ristoikonen@gmail.com>
2024-08-09 18:58:43 +1000

    comment and format code

--- modules/gitfind.js
+++ modules/gitfind.js
@@ -1,8 +1,7 @@
 
 import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 
-
-  async function searchGitHub(searchText, repo = "") {
+async function searchGitHub(searchText, repo = "") {
     const token = "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";
 
     const owner = "ristoikonen";
@@ -24,30 +23,30 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
             const q = "q=:{searchtext}+in:file+language:js+repo:"  + owner + "/" + repo;
         }
         
-        const query = "q=:{searchtext}+in:file+language:js+repo:ristoikonen/Prod";
-
-
         const q = "h2+user:ristoikonen+path:/";
 
 
-            const result =  await octokit.rest.search.code({
+        const result =  await octokit.rest.search.code({
             q,
-            });
+        });
 
-            if (result && "data" in result && "items" in result.data) {
+        if (result && "data" in result && "items" in result.data) {
             const items = result.data.items;
+            
             console.log('ok');
+
             for (const i in items) {
                 try {
                     const item = items[i];
                     const url = item.html_url.replace("https://github.com/", "https://raw.githubusercontent.com/").replace("/blob", "");
                     const response = await fetch(url);
-    
+
                     const text = await response.text();
                     console.log(item.html_url);
                     
                     console.log(text);
 
+                    // in case of format specific processing
                     if (!item.html_url.endsWith(".html")) {
 
                         //const text = await response.text();
@@ -65,33 +64,31 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
                         //         }
                         //     }
                         // }
-                    // } else {
-                    //     const node = await response.json();
-                    //     if ((node["swagger"] || node["openapi"])&& node["paths"]) {
-                    //         const filename = "./results/" + item.repository.full_name.replaceAll("/", "_") + "_" + i + "_" + item.name;
-                    //         console.log(filename);
-                    //         writeFileSync(filename, JSON.stringify(node, null, 2));
-                    //     }
+                        // } else {
+                        //     const node = await response.json();
+                        //     if ((node["swagger"] || node["openapi"])&& node["paths"]) {
+                        //         const filename = "./results/" + item.repository.full_name.replaceAll("/", "_") + "_" + i + "_" + item.name;
+                        //         console.log(filename);
+                        //         writeFileSync(filename, JSON.stringify(node, null, 2));
+                        //     }
                     }
                 }
                 catch(e) {
                     console.error(e);
                 }
-            }
-
+            }// for loop
         }
 
-        const response = await octokit.request("GET /search/repositories?q=div&per_page=10", {
-            owner: owner,
-            repo: repo,
-            });
-
-        console.log(`The status of the response is: ${response.status}`)
-        console.log(`The request URL was: ${response.url}`)
-        console.log(`The x-ratelimit-remaining response header is: ${response.headers["x-ratelimit-remaining"]}`)
-        console.log(`The issue title is: ${response.data[0]}`)
-        
-        console.log(response.data);
+        //const query = "q=:{searchtext}+in:file+language:js+repo:ristoikonen/Prod";
+        // const response = await octokit.request("GET /search/repositories?q=div&per_page=10", {
+        //     owner: owner,
+        //     repo: repo,
+        //     });
+        // console.log(`The status of the response is: ${response.status}`)
+        // console.log(`The request URL was: ${response.url}`)
+        // console.log(`The x-ratelimit-remaining response header is: ${response.headers["x-ratelimit-remaining"]}`)
+        // console.log(`The issue title is: ${response.data[0]}`)
+        // console.log(response.data);
 
         }  catch (error) {
             if (error.response) {
